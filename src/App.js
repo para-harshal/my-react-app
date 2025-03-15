1962dec3127db7af4eccd66c1993f7a6d9804814
harshal chauhan <harshalchauhan840@gmail.com>
2025-03-15 15:18:02 +0530

    added 404 handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Home = () => <h1>Welcome to My React App</h1>;
+const About = () => <h1>About Us</h1>;
+const NotFound = () => <h1>404 - Page Not Found</h1>;
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to My React App!</h1>
-      <p>This React app is hosted on AWS S3 🚀</p>
-    </div>
+    <Router>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} /> {/* 404 Route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
